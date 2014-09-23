5090a09ffe7bc43e578f6d7a26f544e05459beb9
xiaohehu <xiaohe.hu@macminiveloper1.neoscape.local>
2014-09-23 09:38:39 -0400

    Initial Commit

---
+++ victoryCenter/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  victoryCenter
+//
+//  Created by Xiaohe Hu on 9/23/14.
+//  Copyright (c) 2014 Neoscape. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ victoryCenterTests/victoryCenterTests.m
@@ -0,0 +1,34 @@
+//
+//  victoryCenterTests.m
+//  victoryCenterTests
+//
+//  Created by Xiaohe Hu on 9/23/14.
+//  Copyright (c) 2014 Neoscape. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface victoryCenterTests : XCTestCase
+
+@end
+
+@implementation victoryCenterTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
