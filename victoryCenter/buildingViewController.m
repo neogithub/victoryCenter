991c7b51f164f4c90b87c667c893a7e4f4dc5d85
xiaohehu <xiaohe.hu@macminiveloper1.neoscape.local>
2014-10-14 11:27:57 -0400

    Added building & figure panel animation

--- victoryCenter/buildingViewController.m
+++ victoryCenter/buildingViewController.m
@@ -12,6 +12,8 @@
 @interface buildingViewController ()
 
 @property (weak, nonatomic) IBOutlet UIImageView *uiiv_bgImg;
+@property (weak, nonatomic) IBOutlet UIView *uiv_bldImgContainer;
+@property (weak, nonatomic) IBOutlet UIView *uiv_statImgContainer;
 
 @property (weak, nonatomic) IBOutlet UIButton *uib_floorPlan;
 @property (weak, nonatomic) IBOutlet UIButton *uib_bldgStats;
@@ -81,23 +83,51 @@
 
 - (void)updateContent:(int)index
 {
+    _uiiv_bgImg.hidden = YES;
+    _uiv_bldImgContainer.hidden = YES;
+    _uiv_bldImgContainer.transform = CGAffineTransformIdentity;
+    _uiv_statImgContainer.hidden = YES;
+    _uiv_statImgContainer.alpha = 0.0;
+    _uiv_statImgContainer.transform = CGAffineTransformIdentity;
     switch (index) {
         case 1: {
-            NSString *url = [[NSBundle mainBundle] pathForResource:@"grfx_bldStat_bg" ofType:@"jpg"];
-            _uiiv_bgImg.image = [UIImage imageWithContentsOfFile:url];
+            _uiv_bldImgContainer.hidden = NO;
+            _uiv_statImgContainer.hidden = NO;
+            
+            // Animation for the building and stat image
+            CGFloat duration = 0.9f;
+            CGFloat damping = 0.6f;
+            CGFloat velocity = 0.7f;
+            // int to hold UIViewAnimationOption
+            NSInteger option;
+            option = UIViewAnimationCurveEaseInOut;
+            [UIView animateWithDuration:duration delay:0 usingSpringWithDamping:damping initialSpringVelocity:velocity options:option animations:^{
+                _uiv_bldImgContainer.transform = CGAffineTransformMakeTranslation(197.0, 0.0);
+
+            } completion:^(BOOL finished){      }];
+            
+            [UIView animateWithDuration:duration*1.5 delay:0 usingSpringWithDamping:damping*0.8 initialSpringVelocity:velocity*0.8 options:option animations:^{
+                _uiv_statImgContainer.alpha = 1.0;
+                _uiv_statImgContainer.transform = CGAffineTransformMakeTranslation(356, 0.0);
+                
+            } completion:^(BOOL finished){      }];
+            
             break;
         }
         case 2: {
+            _uiiv_bgImg.hidden = NO;
             NSString *url = [[NSBundle mainBundle] pathForResource:@"grfx_bldFloorPlan_bg" ofType:@"jpg"];
             _uiiv_bgImg.image = [UIImage imageWithContentsOfFile:url];
             break;
         }
         case 3: {
+            _uiiv_bgImg.hidden = NO;
             NSString *url = [[NSBundle mainBundle] pathForResource:@"grfx_bldAmenities_bg" ofType:@"jpg"];
             _uiiv_bgImg.image = [UIImage imageWithContentsOfFile:url];
             break;
         }
         case 4: {
+            _uiiv_bgImg.hidden = NO;
             NSString *url = [[NSBundle mainBundle] pathForResource:@"grfx_bldElevator_bg" ofType:@"jpg"];
             _uiiv_bgImg.image = [UIImage imageWithContentsOfFile:url];
             break;
@@ -123,6 +153,12 @@
     
     [_uib_elevators removeFromSuperview];
     _uib_elevators = nil;
+    
+    [_uiv_bldImgContainer removeFromSuperview];
+    _uiv_bldImgContainer = nil;
+    
+    [_uiv_statImgContainer removeFromSuperview];
+    _uiv_statImgContainer = nil;
 }
 
 - (void)didReceiveMemoryWarning
