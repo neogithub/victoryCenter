9308b393224cf1ba59ca87ebef5f30cd3ac7ab8c
xiaohehu <xiaohe.hu@macminiveloper1.neoscape.local>
2014-10-17 15:52:20 -0400

    Added road name labels to floor plan page view

--- victoryCenter/Utilities/Floorplan/embDataViewController.m
+++ victoryCenter/Utilities/Floorplan/embDataViewController.m
@@ -81,6 +81,35 @@
 	// plan info data
 	NSString *planName = _dict[@"floorplaninfo"][0][@"floorinfo"][0];
     [self loadInImge:planName];
+    
+    //Added Road Names Labels
+    UILabel *uil_victoryAve = [[UILabel alloc] initWithFrame:CGRectMake(150, 90, 130, 30)];
+    [uil_victoryAve setText:@"VICTORY AVE"];
+    uil_victoryAve.backgroundColor = [UIColor vcBackGroundColor];
+    [uil_victoryAve setFont:[UIFont fontWithName:@"Raleway-Medium" size:13]];
+    [uil_victoryAve setTextColor:[UIColor vcLightBlue]];
+    [uil_victoryAve setContentMode:UIViewContentModeCenter];
+    CGSize size = [uil_victoryAve.text sizeWithAttributes:
+                   @{NSFontAttributeName:
+                         [UIFont fontWithName:@"Raleway-Medium" size:13]}];
+    uil_victoryAve.frame = CGRectMake(uil_victoryAve.frame.origin.x, uil_victoryAve.frame.origin.y, size.width, size.height);
+    [self.view addSubview: uil_victoryAve];
+    
+    UILabel *uil_highMarketSt = [[UILabel alloc] initWithFrame:CGRectMake(0.0, 130, 130, 30)];
+    [uil_highMarketSt setText:@"HIGH MARKET ST"];
+    uil_highMarketSt.backgroundColor = [UIColor vcBackGroundColor];
+    [uil_highMarketSt setFont:[UIFont fontWithName:@"Raleway-Medium" size:13]];
+    [uil_highMarketSt setTextColor:[UIColor vcLightBlue]];
+    [uil_highMarketSt setContentMode:UIViewContentModeCenter];
+    CGSize size1 = [uil_highMarketSt.text sizeWithAttributes:
+                   @{NSFontAttributeName:
+                         [UIFont fontWithName:@"Raleway-Medium" size:13]}];
+    uil_highMarketSt.frame = CGRectMake(uil_highMarketSt.frame.origin.x, uil_highMarketSt.frame.origin.y, size1.width, size1.height);
+    [self.view addSubview: uil_highMarketSt];
+    CGRect oldFrame = uil_highMarketSt.frame;
+    uil_highMarketSt.layer.anchorPoint = CGPointMake(1.0, 1.0);
+    uil_highMarketSt.frame = oldFrame;
+    uil_highMarketSt.transform = CGAffineTransformMakeRotation(-M_PI_2);
 }
 
 -(void)loadInImge:(NSString *)imageName
