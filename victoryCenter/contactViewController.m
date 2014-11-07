7cd861855c98a7f145a3dddf23368d5870dd9b78
xiaohehu <xiaohe.hu@macminiveloper1.neoscape.local>
2014-11-07 10:10:37 -0500

    Added help view to content view

--- victoryCenter/contactViewController.m
+++ victoryCenter/contactViewController.m
@@ -10,12 +10,13 @@
 #import "UIColor+Extensions.h"
 #import <MessageUI/MessageUI.h>
 #import "embEmailData.h"
+#import "CMPopTipView.h"
 
 static float kContactWidth = 233;
 static float kContactHeight = 100;
 static float kContactGap = 4;
 
-@interface contactViewController ()<MFMessageComposeViewControllerDelegate, MFMailComposeViewControllerDelegate>
+@interface contactViewController ()<MFMessageComposeViewControllerDelegate, MFMailComposeViewControllerDelegate, CMPopTipViewDelegate>
 {
     NSArray         *arr_names;
     NSArray         *arr_phone;
@@ -27,6 +28,11 @@ static float kContactGap = 4;
 
 @property (nonatomic, strong) NSMutableArray        *arr_contactCards;
 @property (nonatomic, strong) embEmailData          *emailData;
+// Help tip view
+@property (nonatomic, strong) NSMutableArray                *arr_helpText;
+@property (nonatomic, strong) NSMutableArray                *visiblePopTipViews;
+@property (nonatomic, strong) NSMutableArray                *arr_helpTargetViews;
+@property (nonatomic, strong) UIView                        *uiv_helpContianer;
 @end
 
 @implementation contactViewController
@@ -35,6 +41,7 @@ static float kContactGap = 4;
 {
     self.view.frame = screenRect;
     self.view.clipsToBounds = YES;
+    [self prepareHlepData];
 }
 
 - (void)viewDidLoad
@@ -45,6 +52,7 @@ static float kContactGap = 4;
     [self setTitleView];
     [self setContactCardsGroup];
     [self performSelector:@selector(animateContactCards) withObject:nil afterDelay:0.5];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(hideAndUnhideHelp:) name:@"hideAndUnhideHelp" object:nil];
 }
 
 #pragma  mark - Set top left title label
@@ -143,7 +151,7 @@ static float kContactGap = 4;
 {
     int index = (int)gesture.view.tag;
     NSString *emailAddress = arr_email[index-1];
-    
+    NSLog(@"The view is %@", gesture.view.description);
     _emailData = [[embEmailData alloc] init];
     _emailData.to =[[NSArray alloc] initWithObjects:emailAddress, nil];
     _emailData.subject = nil;
@@ -310,6 +318,111 @@ static float kContactGap = 4;
 - (IBAction)closeBtnTapped:(id)sender
 {
     [[NSNotificationCenter defaultCenter] postNotificationName:@"removeContact" object:nil];
+}
+
+#pragma mark - Add Help view
+- (void)hideAndUnhideHelp:(NSNotification *)pNotification
+{
+    if (_uiv_helpContianer) {
+        [self fadeOutPopViews:nil];
+    }
+    else {
+        [self loadHelpView];
+    }
+}
+
+- (void)prepareHlepData
+{
+    [_arr_helpText removeAllObjects];
+    _arr_helpText = nil;
+    _arr_helpText = [[NSMutableArray alloc] initWithObjects:
+                     @"Tap close button to return.",
+                     @"Tap cell send email.",
+                     nil];
+    
+    [_arr_helpTargetViews removeAllObjects];
+    _arr_helpTargetViews = nil;
+    UIButton *homeBtn = [[UIButton alloc] initWithFrame:CGRectMake(0.0, 0.0, 45.0, 45.0)];
+    UIView *tmp2 = [[UIView alloc] initWithFrame:CGRectMake(860, 305, 233, 100)];
+    _arr_helpTargetViews = [[NSMutableArray alloc] initWithObjects:homeBtn, tmp2, nil];
+}
+
+- (void)dismissAllPopTipViews
+{
+    [_uiv_helpContianer removeFromSuperview];
+    _uiv_helpContianer = nil;
+    for (CMPopTipView *popTipView in self.visiblePopTipViews) {
+        [popTipView dismissAnimated:YES];
+        [self.visiblePopTipViews removeObject:popTipView];
+    }
+}
+
+- (void)loadHelpView
+{
+	[self dismissAllPopTipViews];
+    _uiv_helpContianer = [[UIView alloc] initWithFrame:screenRect];
+    _uiv_helpContianer.alpha = 0.0;
+    for (int i = 0; i < _arr_helpText.count; i++) {
+        NSString *contentMessage = nil;
+        contentMessage = _arr_helpText[i];
+        UIColor *backgroundColor = [UIColor redColor];
+        UIColor *textColor = [UIColor whiteColor];
+        
+        CMPopTipView *popTipView;
+        popTipView = [[CMPopTipView alloc] initWithMessage:contentMessage];
+        popTipView.delegate = self;
+        
+        /* Some options to try.
+         */
+        //popTipView.disableTapToDismiss = YES;
+        //popTipView.preferredPointDirection = PointDirectionUp;
+        //popTipView.hasGradientBackground = NO;
+        //popTipView.cornerRadius = 2.0;
+        //popTipView.sidePadding = 30.0f;
+        //popTipView.topMargin = 20.0f;
+        //popTipView.pointerSize = 50.0f;
+        popTipView.hasShadow = NO;
+        
+        if (backgroundColor && ![backgroundColor isEqual:[NSNull null]]) {
+            popTipView.backgroundColor = backgroundColor;
+        }
+        if (textColor && ![textColor isEqual:[NSNull null]]) {
+            popTipView.textColor = textColor;
+        }
+        
+        popTipView.animation = arc4random() % 2;
+        popTipView.has3DStyle = NO;
+        
+        popTipView.dismissTapAnywhere = NO;
+        //        [popTipView autoDismissAnimated:NO atTimeInterval:3.0];
+        [popTipView presentPointingAtView:_arr_helpTargetViews[i] inView:_uiv_helpContianer animated:YES];
+        
+        [self.visiblePopTipViews addObject:popTipView];
+    }
+    [self.view addSubview: _uiv_helpContianer];
+    UITapGestureRecognizer *tapOnHelp = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(fadeOutPopViews:)];
+    tapOnHelp.numberOfTapsRequired = 1;
+    _uiv_helpContianer.userInteractionEnabled = YES;
+    [_uiv_helpContianer addGestureRecognizer: tapOnHelp];
+    [UIView animateWithDuration:0.33 animations:^{
+        _uiv_helpContianer.alpha = 1.0;
+    }];
+}
+
+- (void)fadeOutPopViews:(UIGestureRecognizer *)gesture
+{
+    [UIView animateWithDuration:0.33 animations:^{
+        _uiv_helpContianer.alpha = 0.0;
+    } completion:^(BOOL finished){
+        [self dismissAllPopTipViews];
+    }];
+}
+
+#pragma mark - CMPopTipViewDelegate methods
+
+- (void)popTipViewWasDismissedByUser:(CMPopTipView *)popTipView
+{
+	[self.visiblePopTipViews removeObject:popTipView];
 }
 
 #pragma mark - Cleaning Memory
