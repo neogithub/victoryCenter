9b63fb876284336440ff6a08376c276a88e2f36f
xiaohehu <xiaohe.hu@macminiveloper1.neoscape.local>
2014-11-18 13:14:23 -0500

    Remove Collapse code

--- victoryCenter/buildingViewController.h
+++ victoryCenter/buildingViewController.h
@@ -7,10 +7,10 @@
 //
 
 #import <UIKit/UIKit.h>
-#import "CollapseClick.h"
 
-@interface buildingViewController : UIViewController<CollapseClickDelegate>
+
+@interface buildingViewController : UIViewController
 {
-    CollapseClick   *theCollapseClick;
+    
 }
 @end
